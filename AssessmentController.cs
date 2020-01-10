acaf00fa492ba78de88cb3814d47c4d69d973c76
testmail111290 <56623083+testmail111290@users.noreply.github.com>
2020-01-10 12:25:34 +0530

    Update AssessmentController.cs

--- AssessmentController.cs
+++ AssessmentController.cs
@@ -1,15 +1,100 @@
-Please copy Resource Group Credentials from the Screen.
+Note: This question is part of a series of questions that present the same scenario. Each question in the series contains a unique solution that might meet the stated goals. Some question sets might have more than one correct solution, while others might not have a correct solution.
+After you answer a question in this section, you will NOT be able to return to it. As a result, these questions will not appear in the review screen.
+You have an Azure environment.
+You need to create a new Azure virtual machine from an Android laptop.
+Solution: You use Bash in Azure Cloud Shell.
+Does this meet the goal?
+A. Yes
+B. No  (ANS)
 
-1. Create Virtual Private Network by command
-az network vnet create --subnet-name Private  --location "East US" --resource-group user-lvodyfwaeoim --name pooja
+HOTSPOT -
+You plan to create an Azure virtual machine.
+You need to identify which storage service must be used to store the data disks of the virtual machine.
+What should you identify? To answer, select the appropriate service in the answer area.
+Hot Area:
+1.Rest Based object staorage for unstructured data (ANS)
+2.File shares that use SMB 3.0 protocol
+3. Tabular data storage
+4. Effective Storgae apps according to traffic.
 
-2. Create NSG Command
-az network nsg create  --resource-group user-uqccsuhwveds --name pooja 
+Your company plans to move several servers to Azure.
+The companyâ€™s compliance policy states that a server named FinServer must be on a separate network segment.
+You are evaluating which Azure services can be used to meet the compliance policy requirements.
+Which Azure solution should you recommend?
+A. a resource group for FinServer and another resource group for all the other servers
+B. a virtual network for FinServer and another virtual network for all the other servers (ANS)
+C. a VPN for FinServer and a virtual network gateway for each other server
+D. one resource group for all the servers and a resource lock for FinServer
 
-az vm create \
-    --resource-group  user-lctfmfkrublp \
-    --name poojaVM \
-    --image win2016datacenter \
-    --admin-username azureuser \
-    --admin-password Azurepassword123# \
-    --generate-ssh-keys 
+You plan to map a network drive from several computers that run Windows 10 to Azure Storage. You need to create a storage solution in Azure for the planned mapped drive.
+What should you create?
+A. an Azure SQL database
+B. a virtual machine data disk
+C. a Files service in a storage account (ANS)
+D. a Blobs service in a storage account
+
+HOTSPOT -
+You plan to implement an Azure database solution.
+You need to implement a database solution that meets the following requirements:
+-> Can add data concurrently from multiple regions
+-> Can store JSON documents
+Which database service should you deploy? To answer, select the appropriate service in the answer area.
+Hot Area:
+Ans- Explanation
+
+Your company plans to migrate all its network resources to Azure.
+You need to start the planning process by exploring Azure.
+What should you create first?
+A. a subscription (ANS)
+B. a resource group
+C. a virtual network
+D. a management group
+
+
+You have an on-premises application that sends email notifications automatically based on a rule.
+You plan to migrate the application to Azure.
+You need to recommend a serverless computing solution for the application.
+What should you include in the recommendation?
+A. a web app
+B. a server image in Azure Marketplace
+C. a logic app(ANS)
+D. an API app
+
+You plan to deploy a website to Azure. The website will be accessed by users worldwide and will host large video files.
+You need to recommend which Azure feature must be used to provide the best video playback experience.
+What should you recommend?
+A. an application gateway
+B. an Azure ExpressRoute circuit
+C. a content delivery network (CDN)(ANS)
+D. an Azure Traffic Manager profile
+
+
+Your company plans to deploy several million sensors that will upload data to Azure.
+You need to identify which Azure resources must be created to support the planned solution.
+Which two Azure resources should you identify? Each correct answer presents part of the solution.
+NOTE: Each correct selection is worth one point.
+A. Azure Data Lake
+B. Azure Queue storage(ANS)
+C. Azure File Storage
+D. Azure IoT Hub(ANS)
+E. Azure Notification Hubs
+
+You have an Azure web app.
+You need to manage the settings of the web app from an iPhone.
+What are two Azure management tools that you can use? Each correct answer presents a complete solution.
+NOTE: Each correct selection is worth one point.
+A. Azure CLI(ANS)
+B. the Azure portal
+C. Azure Cloud Shell(ANS)
+D. Windows PowerShell
+E. Azure Storage Explorer
+
+This question requires that you evaluate the underlined text to determine if it is correct.
+You have an application that is comprised of an Azure web app that has a Service Level Agreement (SLA) of 99.95 percent and an Azure SQL database that has an SLA of 99.99 percent.
+The composite SLA for the application is the product of both SLAs, which equals 99.94 percent.
+Instructions: Review the underlined text. If it makes the statement correct, select â€œNo change is neededâ€. If the statement is incorrect, select the answer choice that makes the statement correct.
+A. No change is needed (ANs)
+B. the lowest SLA associated to the application, which is 99.95 percent
+C. the highest SLA associated to the application, which is 99.99 percent
+D. the difference between the two SLAs, which is 0.05 percent
+
